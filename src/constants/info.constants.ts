d96dad4bb52c4edd89552c3397914a133ed40d65
unknown <ami145388@gmail.com>
2025-04-03 10:49:38 +0500

    added: new slider images

--- src/constants/info.constants.ts
+++ src/constants/info.constants.ts
@@ -1,25 +1,18 @@
 import { Bath, Clock, Utensils } from 'lucide-react';
 
-import {
-  TActivitiesInfo,
-  TDescriptionInfo,
-  TPricesInfo,
-} from '@/types/info.types';
+import { TDescriptionInfo, TPricesInfo } from '@/types/info.types';
 
 import antlerClinic from '@/assets/img/antlerClinic.png';
 import bath from '@/assets/img/bath.png';
 import bathhouse2 from '@/assets/img/bathhouse2.png';
 import bathhouse from '@/assets/img/bathhouse.png';
-import berries from '@/assets/img/berries.png';
 import cabins from '@/assets/img/cabins.png';
 import corral from '@/assets/img/corral.png';
-import deer from '@/assets/img/deer.png';
 import deers from '@/assets/img/deers.png';
 import doubleRoom2 from '@/assets/img/doubleRoom2.png';
 import doubleRoom from '@/assets/img/doubleRoom.png';
 import field from '@/assets/img/field.png';
 import hallway from '@/assets/img/hallway.png';
-import horses from '@/assets/img/horses.png';
 import house2 from '@/assets/img/house2.png';
 import house from '@/assets/img/house.png';
 import kitchen from '@/assets/img/kitchen.png';
@@ -30,8 +23,24 @@ import livingRoom from '@/assets/img/livingRoom.png';
 import outside2 from '@/assets/img/outside2.png';
 import outside from '@/assets/img/outside.png';
 import river from '@/assets/img/river.png';
+import room1 from '@/assets/img/room1.png';
+import room2 from '@/assets/img/room2.png';
+import room3 from '@/assets/img/room3.png';
+import room4 from '@/assets/img/room4.png';
+import room5 from '@/assets/img/room5.png';
+import room6 from '@/assets/img/room6.png';
+import room7 from '@/assets/img/room7.png';
+import room8 from '@/assets/img/room8.png';
+import room9 from '@/assets/img/room9.png';
+import room10 from '@/assets/img/room10.png';
+import room11 from '@/assets/img/room11.png';
+import room12 from '@/assets/img/room12.png';
 import sign from '@/assets/img/sign.png';
 import stone from '@/assets/img/stone.png';
+
+// import berries from '@/assets/img/berries.png';
+// import deer from '@/assets/img/deer.png';
+// import horses from '@/assets/img/horses.png';
 
 export const TELS = ['+77052636337', '+77055223333', '+77075449796'];
 
@@ -51,10 +60,6 @@ export const SLIDER_IMAGES = [
   antlerClinic,
   house,
   house2,
-  doubleRoom,
-  doubleRoom2,
-  livingRoom,
-  livingRoom2,
   hallway,
   kitchen,
   cabins,
@@ -62,6 +67,22 @@ export const SLIDER_IMAGES = [
   bathhouse,
   bathhouse2,
   deers,
+  livingRoom,
+  livingRoom2,
+  doubleRoom,
+  doubleRoom2,
+  room1,
+  room2,
+  room3,
+  room4,
+  room5,
+  room6,
+  room7,
+  room8,
+  room9,
+  room10,
+  room11,
+  room12,
 ];
 
 export const DESCRIPTION_INFO: TDescriptionInfo[] = [
@@ -105,23 +126,23 @@ export const PRICES_INFO: TPricesInfo[] = [
   // },
 ];
 
-export const ACTIVITIES_INFO: TActivitiesInfo[] = [
-  {
-    img: horses,
-    title: 'Конные походы',
-    description:
-      'Мы организуем конные прогулки, поездки на автомобиле к интересным природным объектам Катон-Карагайского национального природного парка, а так же туры к удивительной горе Белуха (4506 м)',
-  },
-  {
-    img: berries,
-    title: 'Сбор грибов, ягод, трав',
-    description:
-      'Сбор ягод, грибов, лекарственных растений является увлекательным видом отдыха, который имеет релаксирующее воздействие. Не упустите возможность заготовить себе ягоды и травы на зиму.',
-  },
-  {
-    img: deer,
-    title: 'Пантолечение',
-    description:
-      'В биохимический состав пантов марала входят многочисленные аминокислоты (общие и свободные), коллаген, витамины, микро- и макроэлементы, оказывающие положительное влияние на работу всего организма',
-  },
-];
+// export const ACTIVITIES_INFO: TActivitiesInfo[] = [
+//   {
+//     img: horses,
+//     title: 'Конные походы',
+//     description:
+//       'Мы организуем конные прогулки, поездки на автомобиле к интересным природным объектам Катон-Карагайского национального природного парка, а так же туры к удивительной горе Белуха (4506 м)',
+//   },
+//   {
+//     img: berries,
+//     title: 'Сбор грибов, ягод, трав',
+//     description:
+//       'Сбор ягод, грибов, лекарственных растений является увлекательным видом отдыха, который имеет релаксирующее воздействие. Не упустите возможность заготовить себе ягоды и травы на зиму.',
+//   },
+//   {
+//     img: deer,
+//     title: 'Пантолечение',
+//     description:
+//       'В биохимический состав пантов марала входят многочисленные аминокислоты (общие и свободные), коллаген, витамины, микро- и макроэлементы, оказывающие положительное влияние на работу всего организма',
+//   },
+// ];
