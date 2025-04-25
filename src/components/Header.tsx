98e8e6fdffcedf23a6b0593ceeec8e15ed2d406a
unknown <ami145388@gmail.com>
2025-04-25 10:25:46 +0500

    upd: header, fixed: text colors in light themes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ export function Header() {
         ref={ref}
         className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-6xl font-bold text-text-light sm:text-7xl lg:text-8xl ${opacityClass}`}
       >
-        Aq Qaiyŋ
+        Ак-Кайын
         <small className="mt-2 block text-4xl tracking-wider">
           Катон-Карагай
         </small>
